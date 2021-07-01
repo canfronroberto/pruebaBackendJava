6ed810025787816def8b752704f534085fb017ea
jdmm <canfrondj55@gmail.com>
2021-07-01 17:53:14 +0200

    Subida proyecto backend

---
+++ src/main/java/com/endpoint/pruebaBackendJava/PruebaBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package com.endpoint.pruebaBackendJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaBackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaBackendJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/endpoint/pruebaBackendJava/PruebaBackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endpoint.pruebaBackendJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaBackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
